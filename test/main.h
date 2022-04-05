2976aa2ede7c78475ca5b8dab4ab5e809fa63bca
FloLys <4440@holbertonschool.com>
2022-04-05 10:26:46 -0700

    upload main.h

--- test/main.h
+++ test/main.h
@@ -3,7 +3,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
-int main(int ac, char **av);
+int main(void);
+char **getline_token(void);
+char **token_to_av(char *str, char *delim);
+int token_cn(char *str, char *delim);
 
 #endif
