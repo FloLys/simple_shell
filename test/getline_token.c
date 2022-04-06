ca62424126eb6450937c31b7bd485f2bc01f48ff
ayamorcito <izquierdoz.g@gmail.com>
2022-04-06 10:31:40 -0700

    SSS funcionando, con leaks pero funcionando :D

--- test/getline_token.c
+++ test/getline_token.c
@@ -2,28 +2,18 @@
 
 int main(void)
 {
-	char **index = NULL;
-/*	int i;
-*/
-	index = getline_token();
-/*
-	for (i = 0; index[i] != NULL; i++)
-		printf("%s\n", index[i]);	
-
-*/	free(index);
-	return(0);
-}
-
-char **getline_token(void)
-{
 	char *buffer;
 	char eof[4]="EOF";
 	char end[12]="end-of-file";
 	size_t bufsize = 32;
-	int characters = 0, i;
+	int characters, i = 0;
 	char **index = NULL;
+	char *cmd = "/bin/";
+	/*char *av[];*/
+	/*char *env[] = {"/bin", NULL};*/
+	pid_t child_pid;
 
-	buffer = (char *)malloc(bufsize * sizeof(char));
+	/*buffer = (char *)malloc(bufsize * sizeof(char));*/
 	if(buffer == NULL)
 	{
 		perror("Unable to allocate buffer");
@@ -36,13 +26,27 @@ char **getline_token(void)
 		characters = getline(&buffer, &bufsize, stdin);
 		index = token_to_av(buffer, " ");
 
-		for (i = 0; index[i] != NULL; i++)
-			printf("%s\n", index[i]);	
+		child_pid = fork();
+	
+	if (child_pid == -1)
+	{
+		perror("Error:");
+		exit(1);
+	}
+	
+	if (child_pid == 0)
+	{
+	/*	av[index] = *index; */
+		execve(index[0], index, NULL);
+		return (0);
+	}
+	wait(NULL);	
 
 		if (strncmp(buffer, end, 11) == 0 || strncmp(buffer, eof, 3) == 0 || buffer == 0)
 			break;
 	}
 
 	free (buffer);
-	return(index);
+	
+	
 }
--- test/main.h
+++ test/main.h
@@ -4,10 +4,13 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main(void);
-char **getline_token(void);
+/*char **getline_token(void);*/
 char **token_to_av(char *str, char *delim);
 int token_cn(char *str, char *delim);
+/*int execve(char *const pathname[], char *const argv[], char *const envp[]); */
 
 #endif
