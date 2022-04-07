b61322141176bb15b17d1fd81a16377c528aab4f
FloLys <4440@holbertonschool.com>
2022-04-07 13:37:35 -0700

    ssh ctrl d corrected

--- test/getline_token.c
+++ test/getline_token.c
@@ -6,11 +6,8 @@ int main(void)
 	char eof[4]="EOF";
 	char end[12]="end-of-file";
 	size_t bufsize = 32;
-	int characters;/* i = 0;*/
+	int characters;
 	char **index = NULL;
-	/*char *cmd = "/bin/";*/
-	/*char *av[];*/
-	/*char *env[] = {"/bin", NULL};*/
 	pid_t child_pid;
 
 	buffer = (char *)malloc(bufsize * sizeof(char));
@@ -24,29 +21,31 @@ int main(void)
 	{
 		printf("SS$: ");
 		characters = getline(&buffer, &bufsize, stdin);
+		if (characters == -1)
+			break;
+
 		index = token_to_av(buffer, " ");
 
 		child_pid = fork();
-	
-	if (child_pid == -1)
-	{
-		perror("Error:");
-		exit(1);
-	}
-	
-	if (child_pid == 0)
-	{
-	/*	av[index] = *index; */
-		execve(index[0], index, NULL);
-		return (0);
-	}
-	wait(NULL);	
+
+		if (child_pid == -1)
+		{
+			perror("Error:");
+			exit(1);
+		}
+
+		if (child_pid == 0)
+		{
+			execve(index[0], index, NULL);
+			return (0);
+		}
+		wait(NULL);	
 
 		if (strncmp(buffer, end, 11) == 0 || strncmp(buffer, eof, 3) == 0 || buffer == 0)
 			break;
 	}
 
 	free (buffer);
-return (0);	
-	
+	return (0);	
+
 }
