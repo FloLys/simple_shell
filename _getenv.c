019e680511ccf17b8ae3f94640073a22ed28fc5c
FloLys <4440@holbertonschool.com>
2022-04-11 06:55:28 -0700

    style corrected

--- _getenv.c
+++ _getenv.c
@@ -1,9 +1,13 @@
 #include "main.h"
 
+/**
+* _getenv - gets from external environ index the corresponding to input
+* @var: environment to check in
+* Return: string path or null otherwise
+*/
 char *_getenv(const char *var)
 {
 	char *path = NULL;
-	extern char **environ;
 	int i, j = 0, a = 0, flag, auxj;
 
 	for (i = 0; *environ[i] != '\0'; i++)
@@ -18,21 +22,18 @@ char *_getenv(const char *var)
 			else
 				flag = 1;
 		}
-
 		if (flag == 1)
 		{
 			if (environ[i][j] == '=')
 			{
 				auxj = j + 1;
-				for (;environ[i][j + 1] != '\0'; j++);
-
+				for (; environ[i][j + 1] != '\0'; j++)
+				;
 				path = malloc(sizeof(char) * (j + 1));
 				if (path == NULL)
-				{
 					exit(1);
-				}
 
-				for (;environ[i][auxj] != '\0'; auxj++)
+				for (; environ[i][auxj] != '\0'; auxj++)
 				{
 					path[a] = environ[i][auxj];
 					a++;
@@ -42,5 +43,5 @@ char *_getenv(const char *var)
 		}
 	}
 	free(path);
-	return(NULL);
+	return (NULL);
 }
